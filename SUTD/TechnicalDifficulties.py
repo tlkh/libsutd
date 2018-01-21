2686ba3854c57916a5c161fe637e7971abaf94ff
Tong Hui Kang <tonghuikang@outlook.com>
2018-01-22 00:47:16 +0800

    fire alarm added
    lolz

--- SUTD/TechnicalDifficulties.py
+++ SUTD/TechnicalDifficulties.py
@@ -1,13 +1,22 @@
 """
 Part and parcel of daily life?
+lol why the fuck only got 2 spaces per indent
 """
 
-def projector()
+
+def projector():
+  """9 per cohort class"""
   switch_on = "Nope. Please call tech support"
   raise Exception(switch_on)
 
-def air_con(temp)
+def fire_alarm():
+  '''random'''
+  for _ in range(10):
+    print("Ladies and gentlemen, it was a false alarm. I repeat, it was a false alarm. Sorry for the inconvenience caused.")
+
+def air_con(temp):
+  '''singapore so hot'''
   if temp > 25:
     print("TURN IT DOWN AND SAVE POWER")
-   else:
+  else:
     print("Please note it is centrally controlled and it will be set at 21 degrees") 
