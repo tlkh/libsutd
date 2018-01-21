b4bfe72a99f824d81fe95e926ba80bd1ec9d3181
Timothy Liu <tlkh@users.noreply.github.com>
2018-01-21 21:57:59 +0800

    initial commit

---
+++ SUTD.py
@@ -0,0 +1,35 @@
+class DigitalWorld:
+    """A better world by Design, one line of code at a time."""
+    
+    #def __init__(self):
+
+    def omigerd(error_type):
+        import webbrowser
+        url = "https://stackexchange.com/search?q=" + str(error_type)
+        webbrowser.open(url, new=0, autoraise=True)
+
+class Grumpy:
+    """Just why? :("""
+
+    grumpy_tomb = """
+                    _  /)
+                    mo / )
+                    |/)\)
+                    /\_
+                    \__|=
+                    (    )
+                    __)(__
+            _____/      \\_____
+            |  _     ___   _   ||
+            | | \     |   | \  ||
+            | |  |    |   |  | ||
+            | |_/     |   |_/  ||
+            | | \     |   |    ||
+            | |  \    |   |    ||
+            | |   \. _|_. | .  ||
+            |                  ||
+            |      GRUMPY      ||
+            |                  ||
+    *       | *   **    * **   |**      **
+    \))ejm97/.,(//,,..,,\||(,,.,\\,.((//"""
+    print(grumpy_tomb)
---
+++ test.py
@@ -0,0 +1,7 @@
+from SUTD import DigitalWorld as dw
+from SUTD import Grumpy
+
+try:
+    fail = (1,2) + lol
+except Exception as e:
+    dw.omigerd(e)
